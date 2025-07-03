495ae1995f7969b4be02d7ced005909dcabf5bb2
Rafi <itzmeh320@gmail.com>
2025-07-04 00:53:42 +0600

    Cors

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,18 @@ import express, {Application, Request, Response} from 'express'
 import { config } from 'dotenv';
 import { booksRoute } from './controllers/books.controller';
 import { borrowRoute } from './controllers/borrow.controller';
-
+import cors from 'cors';
 const app: Application = express();
 config();
 
 app.use(express.json())
 app.use("/api",booksRoute)
 app.use("/api",borrowRoute)
+app.use(
+  cors({
+    origin: ['https://library-app-red-gamma.vercel.app/']
+   })
+);
 
 app.get('/',(req:Request,res:Response)=>{
     res.send('Library Management System')
